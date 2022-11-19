de9576d6e8ff891304cd04db823ed55c1dd94b5e
Ben Capper <Bencapper19@gmail.com>
2022-11-19 19:10:24 +0000

    Trending tv tests
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- cypress/e2e/trendingtv.cy.js
+++ cypress/e2e/trendingtv.cy.js
@@ -4,10 +4,10 @@ let page = 1;
 const email = "test@test.com"; 
 const pass = "test20"; 
 
-describe("The Trending Movies page", () => {
+describe("The Trending TV page", () => {
   before(() => {
     cy.request(
-      `https://api.themoviedb.org/3/trending/movie/week?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=${page}`
+      `https://api.themoviedb.org/3/trending/tv/week?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=${page}`
     )
       .its("body")
       .then((response) => {
@@ -17,29 +17,28 @@ describe("The Trending Movies page", () => {
   });
   beforeEach(() => {
     cy.login(email,pass);
-    cy.visit(`/movies/trending`);
+    cy.visit(`/tv/trending`);
   });
-  it("Displays trending header and 20 trending movies on page 1", () => {
-    cy.get('h3').contains("Trending Movies");
+  it("Displays trending tv header and 20 trending tv on page 1", () => {
+    cy.get('h3').contains("Trending TV");
     cy.get(".MuiCardHeader-root").should("have.length", 20);
     cy.get("li").eq(1).contains("1");
   });
-  it("Tests whether the trending movies are correct", () => {
+  it("Tests whether the trending tv are correct", () => {
       cy.get(".MuiCardHeader-root")
         .within(() => {
           cy.get("p").each(($card, index) => {
-            cy.wrap($card).contains(trending[index].title);
+            cy.wrap($card).contains(trending[index].name);
           });
         });
     });
-  it("Tests clicking to next page of trending movies", () => {
+  it("Tests clicking to next page of trending tv", () => {
     cy.get("li").eq(2).contains("2").click();
     page = 2;
     cy.get(".MuiCardHeader-root")
       .within(() => {
         cy.get("p").each(($card, index) => {
-          //cy.log(trending[index].title);
-          cy.wrap($card).contains(trending[index].title);
+          cy.wrap($card).contains(trending[index].name);
       });
     });
   });
